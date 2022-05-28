2b2c1d2c1e396d6c2e6af997c71cb4cdc8bf352c
victorVerlaenen <vverlaenen@gmail.com>
2022-05-28 21:12:59 +0200

    working on sound system
    still figuring out how my play funciton should get the right sound

--- Minigin/Audio.cpp
+++ Minigin/Audio.cpp
@@ -1,2 +1,93 @@
 #include "MiniginPCH.h"
 #include "Audio.h"
+
+dae::Audio::Audio(const std::string& filePath)
+{
+	pImlp = new AudioImpl{ filePath };
+}
+
+dae::Audio::AudioImpl::AudioImpl(const std::string& filePath)
+	:m_FilePath(filePath)
+{
+
+}
+
+dae::Audio::~Audio()
+{
+	delete pImlp;
+	pImlp = nullptr;
+}
+
+dae::Audio::AudioImpl::~AudioImpl()
+{
+	if(m_pMixChunk != nullptr)
+	{
+		Mix_FreeChunk(m_pMixChunk);
+	}
+}
+
+void dae::Audio::Play() const
+{
+	pImlp->Play();
+}
+
+void dae::Audio::AudioImpl::Play()
+{
+	if(IsLoaded() == false)
+	{
+		return;
+	}
+	Mix_PlayChannel(-1, m_pMixChunk, 0);
+}
+
+void dae::Audio::Load() const
+{
+	pImlp->Load();
+}
+
+
+void dae::Audio::AudioImpl::Load()
+{
+	m_pMixChunk = Mix_LoadWAV(m_FilePath.c_str());
+}
+
+void dae::Audio::SetVolume(float volume)
+{
+	pImlp->SetVolume(volume);
+}
+
+void dae::Audio::AudioImpl::SetVolume(int volume)
+{
+	if(IsLoaded())
+	{
+		Mix_Volume(-1, volume);
+	}
+}
+
+int dae::Audio::GetVolume() const
+{
+	return pImlp->GetVolume();
+}
+
+int dae::Audio::AudioImpl::GetVolume() const
+{
+	if(IsLoaded())
+	{
+		return Mix_VolumeChunk(m_pMixChunk, -1);
+	}
+	return 0;
+}
+
+bool dae::Audio::IsLoaded() const
+{
+	return pImlp->IsLoaded();
+}
+
+bool dae::Audio::AudioImpl::IsLoaded() const
+{
+	if(m_pMixChunk != nullptr)
+	{
+		return true;
+	}
+	return false;
+}
--- Minigin/Audio.h
+++ Minigin/Audio.h
@@ -1,11 +1,13 @@
 #pragma once
+#include "SDL_mixer.h"
+
 namespace dae
 {
 	class Audio final
 	{
 	public:
-		Audio() = default;
-		~Audio() = default;
+		Audio(const std::string& filePath);
+		~Audio();
 
 		Audio(const Audio& other) = delete;
 		Audio(Audio&& other) noexcept = delete;
@@ -17,24 +19,31 @@ namespace dae
 
 		void SetVolume(float volume);
 		int GetVolume() const;
+		bool IsLoaded() const;
 	private:
 		class AudioImpl;
 		AudioImpl* pImlp{ };
 	};
+	
+	class Audio::AudioImpl
+	{
+	public:
+		AudioImpl(const std::string& filePath);
+		~AudioImpl();
+
+		AudioImpl(const AudioImpl& other) = delete;
+		AudioImpl(AudioImpl&& other) noexcept = delete;
+		AudioImpl& operator=(const AudioImpl& other) = delete;
+		AudioImpl& operator=(AudioImpl&& other) noexcept = delete;
+
+		void Play();
+		void Load();
+
+		void SetVolume(int volume);
+		int GetVolume() const;
+		bool IsLoaded() const;
+	private:
+		Mix_Chunk* m_pMixChunk{};
+		std::string m_FilePath;
+	};
 }
-
-class dae::Audio::AudioImpl
-{
-public:
-	AudioImpl(const std::string& path);
-	~AudioImpl();
-
-	bool Play();
-	void Load();
-
-	void SetVolume(int volume);
-	int GetVolume() const;
-private:
-	//Mix_Chunk* m_pChunk{};
-	std::string m_Path;
-};
